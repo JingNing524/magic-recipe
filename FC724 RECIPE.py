db427ad33d9f47ab0cb6dc3ec15ceaf39aa3f37c
Lynn <jning000000524@gmail.com>
2025-05-01 14:39:05 +0100

    create Ingredient class

--- FC724 RECIPE.py
+++ FC724 RECIPE.py
@@ -1,1 +1,11 @@
 #Interactive Recipe Manager
+
+
+class Ingredient:
+    def __init__(self, name, quantity, unit):
+        self.name = name
+        self.quantity = quantity
+        self.unit = unit
+
+    def display(self):
+        return f"{self.quantity} {self.unit} {self.name}"
